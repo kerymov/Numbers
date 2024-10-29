f296a8dce2fa43824243bc3fc536d91a31016054
Ilya Kerymov <illya.kerymov31@gmail.com>
2024-10-29 02:08:02 +0200

    Edit typography text styles.

--- app/src/main/java/com/example/numbers/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/example/numbers/presentation/ui/theme/Type.kt
@@ -17,21 +17,21 @@ val montserrat = FontFamily(
 val Typography = Typography(
     titleLarge = TextStyle(
         fontFamily = montserrat,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
+        fontWeight = FontWeight.Bold,
+        fontSize = 28.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
     titleMedium = TextStyle(
         fontFamily = montserrat,
-        fontWeight = FontWeight.Medium,
+        fontWeight = FontWeight.SemiBold,
         fontSize = 18.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.15.sp
     ),
     labelLarge = TextStyle(
         fontFamily = montserrat,
-        fontWeight = FontWeight.Medium,
+        fontWeight = FontWeight.SemiBold,
         fontSize = 14.sp,
         lineHeight = 20.sp,
         letterSpacing = 0.1.sp
