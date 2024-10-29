40450e91d67518a5c2d23808982de01cfeea2b72
Ilya Kerymov <illya.kerymov31@gmail.com>
2024-10-29 17:02:47 +0200

    Add model mappers.

---
+++ app/src/main/java/com/example/numbers/data/mappers/Mappers.kt
@@ -0,0 +1,12 @@
+package com.example.numbers.data.mappers
+
+import com.example.numbers.data.database.FactEntity
+import com.example.numbers.data.models.Fact
+
+fun FactEntity.mapToFact(): Fact {
+    return Fact(
+        id = this.id,
+        number = this.number,
+        fact = this.fact
+    )
+}
---
+++ app/src/main/java/com/example/numbers/presentation/mappers/Mappers.kt
@@ -0,0 +1,12 @@
+package com.example.numbers.presentation.mappers
+
+import com.example.numbers.data.models.Fact
+import com.example.numbers.presentation.ui.models.FactUi
+
+fun Fact.mapToFactUi(): FactUi {
+    return FactUi(
+        id = this.id,
+        number = this.number,
+        fact = this.fact
+    )
+}
