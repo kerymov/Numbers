58295e3721508ec6363607d9ddcb1d8d15f47893
Ilya Kerymov <illya.kerymov31@gmail.com>
2024-10-29 02:07:27 +0200

    Fix FactDao.

--- app/src/main/java/com/example/numbers/data/database/FactDao.kt
+++ app/src/main/java/com/example/numbers/data/database/FactDao.kt
@@ -10,6 +10,6 @@ interface FactDao {
     @Insert
     fun insertFact(fact: FactEntity)
 
-    @Query("SELECT * FROM search_history")
+    @Query("SELECT * FROM search_history WHERE id = :id")
     fun getFactById(id: Long): FactEntity
 }
